111fae2950978a49dfa5502aa624973fa7e2d255
p1kachU-gav <riel8383@gmail.com>
2024-12-31 18:58:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript to handle button click
+document.getElementById("contactButton").addEventListener("click", function() {
+    alert("Thank you for clicking the button! We'll get back to you soon.");
+});
